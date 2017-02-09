e6bc77a65df4d6b407616c4e05e1d23992a212c2
ArachisH <HuitronAJ@gmail.com>
2017-02-08 19:52:11 -0800

    Cleaned up some code that relied on a removed property 'OutputDirectoryName', will now use Options.OutputDirectory. Added remote client downloader thingy.

--- HabBit/Program.cs
+++ HabBit/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
@@ -16,7 +17,16 @@ namespace HabBit
 {
     public class Program
     {
-        public HGame Game { get; }
+        private const string EXTERNAL_VARIABLES_URL =
+            "https://www.habbo.com/gamedata/external_variables";
+
+        private const string FLASH_CLIENT_URL_FORMAT =
+            "http://habboo-a.akamaihd.net/gordon/{0}/Habbo.swf";
+
+        private const string CHROME_USER_AGENT =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+
+        public HGame Game { get; set; }
         public HBOptions Options { get; }
 
         public bool IsModifying
@@ -36,16 +46,17 @@ namespace HabBit
         {
             get { return Options.IsDumpingMessageData; }
         }
-        public string OutputDirectoryName { get; private set; }
 
         public Program(string[] args)
         {
             Options = new HBOptions(args);
-            Game = new HGame(Options.ClientInfo.FullName);
-
-            if (Options.Compression == null)
+            if (!Options.IsFetchingClient)
             {
-                Options.Compression = Game.Compression;
+                Game = new HGame(Options.ClientInfo.FullName);
+                if (Options.Compression == null)
+                {
+                    Options.Compression = Game.Compression;
+                }
             }
         }
         public static void Main(string[] args)
@@ -64,6 +75,11 @@ namespace HabBit
 
         private void Run()
         {
+            if (Options.IsFetchingClient)
+            {
+                Fetch();
+            }
+
             Disassemble();
             if (IsModifying)
             {
@@ -74,6 +90,54 @@ namespace HabBit
                 Extract();
             }
             Assemble();
+        }
+        private void Fetch()
+        {
+            string title = "Fetching Client";
+            bool isRevisionKnown = !(string.IsNullOrWhiteSpace(Options.RemoteRevision));
+
+            if (isRevisionKnown)
+            {
+                title += $"({Options.RemoteRevision})";
+            }
+            ConsoleEx.WriteLineTitle("Fetching Client");
+
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.UserAgent] = CHROME_USER_AGENT;
+                if (!isRevisionKnown)
+                {
+                    using (var gameDataStream = new StreamReader(client.OpenRead(EXTERNAL_VARIABLES_URL)))
+                    {
+                        while (!gameDataStream.EndOfStream)
+                        {
+                            // Maybe we can use some of this other stuff later, ignore for now though.
+                            string line = gameDataStream.ReadLine();
+                            if (!line.StartsWith("flash.client.url")) continue;
+
+                            int revisionStart = (line.IndexOf("gordon/") + 7);
+                            Options.RemoteRevision = line.Substring(revisionStart, (line.Length - revisionStart) - 1);
+
+                            Console.WriteLine("Latest Revision: " + Options.RemoteRevision);
+                            break;
+                        }
+                    }
+                }
+
+                var remoteUri = new Uri(string.Format(FLASH_CLIENT_URL_FORMAT, Options.RemoteRevision));
+                Options.ClientInfo = new FileInfo(Path.Combine(Options.OutputDirectory, remoteUri.LocalPath.Substring(8)));
+                Options.OutputDirectory = Directory.CreateDirectory(Options.ClientInfo.DirectoryName).FullName;
+
+                Console.Write($"Downloading Client({Options.RemoteRevision})...");
+                client.DownloadFile(remoteUri, Options.ClientInfo.FullName);
+                ConsoleEx.WriteLineFinished();
+            }
+
+            Game = new HGame(Options.ClientInfo.FullName);
+            if (Options.Compression == null)
+            {
+                Options.Compression = Game.Compression;
+            }
         }
         private void Modify()
         {
@@ -126,8 +190,6 @@ namespace HabBit
                 Game.Revision = Options.Revision;
                 ConsoleEx.WriteLineChanged("Revision Changed", oldRevision, Game.Revision);
             }
-
-            Console.WriteLine();
         }
         private void Extract()
         {
@@ -136,14 +198,12 @@ namespace HabBit
             Console.Write("Generating Message Profiles...");
             Game.GenerateMessageProfiles();
             ConsoleEx.WriteLineFinished();
-
-            Console.WriteLine();
         }
         private void Assemble()
         {
             ConsoleEx.WriteLineTitle("Assembling");
 
-            string asmdPath = Path.Combine(OutputDirectoryName, Options.ClientInfo.Name);
+            string asmdPath = Path.Combine(Options.OutputDirectory, ("asmd_" + Options.ClientInfo.Name));
             using (var asmdStream = File.Open(asmdPath, FileMode.Create))
             using (var asmdOutput = new FlashWriter(asmdStream))
             {
@@ -153,7 +213,7 @@ namespace HabBit
 
             if (Options.IsReplacingRSAKeys)
             {
-                string keysPath = Path.Combine(Path.Combine(OutputDirectoryName, "RSAKeys.txt"));
+                string keysPath = Path.Combine(Options.OutputDirectory, "RSAKeys.txt");
                 using (var keysStream = File.Open(keysPath, FileMode.Create))
                 using (var keysOutput = new StreamWriter(keysStream))
                 {
@@ -166,7 +226,7 @@ namespace HabBit
 
             if (Options.IsDumpingMessageData)
             {
-                string msgsPath = Path.Combine(OutputDirectoryName, "Messages.txt");
+                string msgsPath = Path.Combine(Options.OutputDirectory, "Messages.txt");
                 using (var msgsStream = File.Open(msgsPath, FileMode.Create))
                 using (var msgsOutput = new StreamWriter(msgsStream))
                 {
@@ -177,20 +237,21 @@ namespace HabBit
 
                     msgsOutput.WriteLine("// Incoming Messages | " + Game.OutMessages.Count.ToString("n0"));
                     WriteMessages(msgsOutput, "Incoming", Game.InMessages);
-                    
+
                     Console.WriteLine("Messages Saved: " + msgsPath);
                 }
             }
-
-            Console.WriteLine();
         }
         private void Disassemble()
         {
             ConsoleEx.WriteLineTitle("Disassembling");
             Game.Disassemble();
 
-            OutputDirectoryName = Path.Combine(Options.ClientInfo.DirectoryName, Game.Revision);
-            Directory.CreateDirectory(OutputDirectoryName);
+            if (!Options.OutputDirectory.EndsWith(Game.Revision))
+            {
+                string directoryName = Path.Combine(Options.OutputDirectory, Game.Revision);
+                Options.OutputDirectory = Directory.CreateDirectory(directoryName).FullName;
+            }
 
             var productInfo = (ProductInfoTag)Game.Tags
                 .First(t => t.Kind == TagKind.ProductInfo);
@@ -199,8 +260,6 @@ namespace HabBit
             Console.WriteLine($"Incoming Messages: {Game.InMessages.Count:n0}");
             Console.WriteLine("Compilation Date: {0}", productInfo.CompilationDate);
             Console.WriteLine("Revision: " + Game.Revision);
-
-            Console.WriteLine();
         }
 
         private void WriteMessage(StreamWriter output, HGame.MessageItem message)
@@ -211,7 +270,12 @@ namespace HabBit
             string sha1 = message.Profile.SHA1;
             string constructorSig = instance.Constructor.ToAS3(true);
 
-            output.WriteLine($"[{message.Header}, {sha1}] = {name}{constructorSig}");
+            output.Write($"[{message.Header}, {sha1}] = {name}{constructorSig}");
+            if (!message.IsOutgoing)
+            {
+                output.Write($"[Parser: {message.Parser.Instance.QName.Name}]");
+            }
+            output.WriteLine();
         }
         private void WriteMessages(StreamWriter output, string title, IDictionary<ushort, HGame.MessageItem> messages)
         {
