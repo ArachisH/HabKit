1d1babe280e1c80e848431adbd19a70da2c19bb2
The Old Nut Man <HuitronAJ@gmail.com>
2017-04-24 01:27:12 -0700

    Implemented new command line parsing system.

---
+++ HabBit/Commands/Command.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HabBit.Commands
+{
+    public abstract class Command
+    {
+        public abstract void Populate(Queue<string> parameters);
+    }
+}
---
+++ HabBit/Commands/MatchCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace HabBit.Commands
+{
+    public class MatchCommand : Command
+    {
+        public bool MinimalComments { get; set; }
+        public string Pattern { get; set; } = @"(?<start>(.*?)[^""])(?<id>[+-]?[0-9]\d*(\.\d+)?)\b(?<end>[^\r|$]*)";
+
+        public FileInfo PreviousGameInfo { get; set; }
+        public FileInfo ClientHeadersInfo { get; set; }
+        public FileInfo ServerHeadersInfo { get; set; }
+
+        public override void Populate(Queue<string> parameters)
+        {
+            while (parameters.Count > 0)
+            {
+                string parameter = parameters.Dequeue();
+                switch (parameter)
+                {
+                    case "-p":
+                    {
+                        Pattern = parameters.Dequeue();
+                        break;
+                    }
+                    case "-mc":
+                    {
+                        MinimalComments = true;
+                        break;
+                    }
+                    default:
+                    {
+                        if (PreviousGameInfo == null)
+                        {
+                            PreviousGameInfo = new FileInfo(parameter);
+                        }
+                        else if (ClientHeadersInfo == null)
+                        {
+                            ClientHeadersInfo = new FileInfo(parameter);
+                        }
+                        else if (ServerHeadersInfo == null)
+                        {
+                            ServerHeadersInfo = new FileInfo(parameter);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
