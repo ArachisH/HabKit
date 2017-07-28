3d1525456c95d4f50f5ff767cfa53234e19ae579
ArachisH <HuitronAJ@gmail.com>
2017-07-28 10:06:18 -0700

    Corrected the reference to the Sanitizers type Dropped a nest yo

--- HabBit/Commands/CleanCommand.cs
+++ HabBit/Commands/CleanCommand.cs
@@ -6,22 +6,21 @@ namespace HabBit.Commands
 {
     public class CleanCommand : Command
     {
-        public GSanitizations Sanitizations { get; set; } = (GSanitizations.Deobfuscate | GSanitizations.RegisterRename | GSanitizations.IdentifierRename);
+        public Sanitizers Sanitizations { get; set; } = (Sanitizers.Deobfuscate | Sanitizers.RegisterRename | Sanitizers.IdentifierRename);
 
         public override void Populate(Queue<string> parameters)
         {
-            if (parameters.Count > 0)
+            if (parameters.Count == 0) return;
+            Sanitizations = Sanitizers.None;
+
+            while (parameters.Count > 0)
             {
-                Sanitizations = GSanitizations.None;
-                while (parameters.Count > 0)
+                string parameter = parameters.Dequeue();
+                switch (parameter)
                 {
-                    string parameter = parameters.Dequeue();
-                    switch (parameter)
-                    {
-                        case "-deob": Sanitizations |= GSanitizations.Deobfuscate; break;
-                        case "-rr": Sanitizations |= GSanitizations.RegisterRename; break;
-                        case "-ir": Sanitizations |= GSanitizations.IdentifierRename; break;
-                    }
+                    case "-deob": Sanitizations |= Sanitizers.Deobfuscate; break;
+                    case "-rr": Sanitizations |= Sanitizers.RegisterRename; break;
+                    case "-ir": Sanitizations |= Sanitizers.IdentifierRename; break;
                 }
             }
         }
